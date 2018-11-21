22a00ee09e1a3d9f03add0601c65c9d22a2787ca
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-11-21 12:49:43 -0500

    8.5. ngStyle y su uso con directivas de atributos.

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [style.fontSize.px]="tamano">
+      hola mundo.. esta es una etuqieta
+    </p>
+    <button class="btn btn-primary" (click)="tamano = tamano + 5">
+    <i class="fa fa-plus"></i>
+    </button>
+
+    <button class="btn btn-primary" (click)="tamano = tamano - 5">
+    <i class="fa fa-minus"></i>
+    </button>
+    
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano: number = 30;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
